46f043e17f64ba990b93862d46e6a2bc51b9b5a5
Drew Pappas <drewpappas@mac.com>
2016-03-08 16:50:25 -0800

    updated manifest to support all portal page enviroments

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 // This background script listens for a visit to the Admin Portal Page
 function checkForValidUrl(tabId, changeInfo, tab) {
-  if (tab.url.indexOf('https://admin.lynda.com/admin/portalsetup.aspx') == 0) {
+ if (tab.url.indexOf('https://admin.lynda.com/admin/portalsetup.aspx') == 0) {
     chrome.pageAction.show(tabId);
   }
 };
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
