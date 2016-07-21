5edd31499333e24f43d2dd01dc787c1d51b347ce
Drew Pappas <drewpappas@mac.com>
2016-07-20 17:34:39 -0700

    initial commit

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-// This background script listens for a visit to the Admin Portal Page
+// This background script listens for a visit to the LL Proxy Page
 function checkForValidUrl(tabId, changeInfo, tab) {
- if (tab.url.indexOf('https://admin.lynda.com/admin/portalsetup.aspx') == 0) {
+ if (tab.url.indexOf('http://172.18.123.223/test.html') == 0) {
     chrome.pageAction.show(tabId);
   }
 };
--- injection.js
+++ injection.js
@@ -1,3 +1,48 @@
-$('[name=portalId]').select2({dropdownAutoWidth : true});
-$('.select2-container').css({"width": "80%"});
-$('.select2-results').css({"max-height": "800px"});
+// HTML Components
+var saveBtn = "<input class='btn btn-success' type='button' id='patronApiSave' value='Save'>"
+var savedConfigsPanel = "<div class='panel panel-success' id='savedConfigsPanel'><div class='panel-heading'><h4>Saved Configurations</h4></div><ul class='list-group'></ul></div>"
+
+// Page arrangement code
+$( "#patronApiFields > div:nth-child(10)" ).append(saveBtn);
+$( "#response" ).append(savedConfigsPanel);
+
+// Load saved configs
+for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+  addSavedConfigToPanel( localStorage.key( i ), i );
+}
+loadConfigOnClick();
+
+// On save
+$('#patronApiSave').click(function(){
+  var hostname = $('#patronApiHostname').val();
+  if (hostname === ""){
+   $('#json').append("Invalid hostname.");
+  } else {
+   localStorage[hostname] = captureDebuggerForm();
+   var savedConfigCount = localStorage.length + 1
+   addSavedConfigToPanel(hostname, savedConfigCount);
+  }
+});
+
+function addSavedConfigToPanel(hostname, id){
+  var savedConfigHtml = "<li class='list-group-item config' id=" + id +">" + hostname +"</li>"
+  $('#savedConfigsPanel').append(savedConfigHtml);
+}
+
+function loadConfigOnClick(){
+  $(".config").click(function(){
+    var hostname = $(this).text()
+    console.log(hostname);
+    var savedForm = $.parseHTML(localStorage[hostname])
+    console.log(savedForm);
+    $( "#patronApiFields").replaceWith(savedForm)
+  });
+}
+
+function captureDebuggerForm() {
+  var values = {}
+  $.each($('#debugger').serializeArray(), function(i, field) {
+    values[field.name] = field.value;
+  });
+  return values
+}
